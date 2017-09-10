32e557088913053a97fed546740e4a73b8f0691a
Jorch95 <Jorch95@gmail.com>
2017-09-10 18:38:49 -0300

    De esta forma se lee del archivo y se imprimen los primeros valores

--- Práctica/Ejercicio3/index.py
+++ Práctica/Ejercicio3/index.py
@@ -2,20 +2,23 @@ from flask import Flask
 from flask import request
 from flask import render_template
 import random
+from readfile import devolverValores
 
 #TODO definir inicio
 
 app = Flask(__name__)
 
 filename="datos.txt"
-
+#arch = open(filename, 'r')
 @app.route('/')
 def index():
     # presion = random.randrange(101)
     # temperatura = random.randrange(0,30)
     # humedad = random.randrange(60,95) #Si estamos en La Plata.
     # velocidadViento = random.randrange(60,120)
-    return render_template('response.html',dataP=presion,dataT=temperatura,dataH=humedad,dataV=velocidadViento)
+    arreglo = []
+    arreglo = devolverValores()
+    return render_template('response.html',dataT=arreglo[0],dataH=arreglo[1],dataP=arreglo[2], dataV=arreglo[3])
     
 if __name__ == "__main__": 
     app.run(host='0.0.0.0', port=1111)
--- Práctica/Ejercicio3/readfile.py
+++ Práctica/Ejercicio3/readfile.py
@@ -1,6 +1,8 @@
 # This Python file uses the following encoding: utf-8
 import os, sys, string  
-arch = open("datos.txt",'r')
-for linea in arch.readlines():
-    print linea.split("-")
-
+def devolverValores():
+    arreglo = [] #datos que se devuelven.
+    arch = open("datos.txt",'r')
+    for linea in arch.readlines():
+        arreglo = linea.split("-")
+    return arreglo
